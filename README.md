740e799e2e250d6c49b8679c4d313659d5927b4f
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2022-06-15 19:21:42 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# nftairdrop-subgrap
--- src/rank-rewards-nft.ts
+++ src/rank-rewards-nft.ts
@@ -1,62 +1,24 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  RankRewardsNFT,
-  Approval,
-  ApprovalForAll,
-  Transfer
-} from "../generated/RankRewardsNFT/RankRewardsNFT"
-import { ExampleEntity } from "../generated/schema"
+import { Transfer } from "../generated/RankRewardsNFT/RankRewardsNFT";
+import { NFTAvatar, User } from "../generated/schema";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleTransfer(event: Transfer): void {
+  let entity = new NFTAvatar(event.params.tokenId.toString());
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  const owner = event.params.to;
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let user = User.load(owner.toHexString());
+
+  if (!user) {
+    user = new User(owner.toHexString());
+    user.count = 0;
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  user.count = user.count + 1;
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
+  entity.owner = user.id;
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  entity.transaction = event.transaction.hash;
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.rewardRank(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenURI(...)
+  user.save();
+  entity.save();
 }
-
-export function handleApprovalForAll(event: ApprovalForAll): void {}
-
-export function handleTransfer(event: Transfer): void {}
